59f2e13ef058fed72298512c796b8ae53ba0a899
WARATEP TUBBOON <waratep_tubboon@icloud.com>
2018-11-19 04:55:12 +0700

    update

--- PI.c
+++ PI.c
@@ -1,52 +1,44 @@
 #include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <math.h>
+#define MAX_THREADS 32
 
-#define MAX_THREADS 64
-#define SEED 35791246
-
-
-static long steps = 1000000000;
+static long long steps = 100000000000;
 double step;
 
 int main (int argc, const char *argv[]) {
 
-    long int i,j,count;
+    long long int i,j;
+    double x;
+    double pi, sum = 0.0;
     double start, delta;
-    double x,y,z,pi;
 
     step = 1.0/(double) steps;
 
     // Compute parallel compute times for 1-MAX_THREADS
-    // for (j=1; j<= MAX_THREADS; j++) {
+    for (j = MAX_THREADS; j <= MAX_THREADS; j++) {
 
-        // printf(" running on %d threads: ", j);
+        printf(" running on %lld threads: ", j);
 
         // This is the beginning of a single PI computation 
-        // omp_set_num_threads(j);
+        omp_set_num_threads(j);
 
-        // double start = omp_get_wtime();
+        sum = 0.0;
+        double start = omp_get_wtime();
 
 
-        // #pragma omp parallel for reduction(+:sum) private(x)
-        // #pragma omp parallel default(none) \ private(i,x,y,z) shared(count,steps) 
-        // #pragma omp for
+        #pragma omp parallel for reduction(+:sum) private(x)
         for (i=0; i < steps; i++) {
-            srand(SEED);
-            x = (double)rand()/RAND_MAX;
-            y = (double)rand()/RAND_MAX;
-            z = x*x+y*y;
-            if (z<=1) count++;
-            pi = (double)count/steps*4;
-            printf("step %ld : %.30g\n",i,pi);
-
+            x = (i+0.5)*step;
+            sum += 4.0 / (1.0+x*x); 
         }
 
+        // Out of the parallel region, finialize computation
+        pi = step * sum;
+        delta = omp_get_wtime() - start;
+        printf("PI = %.16g computed in %.4g seconds\n", pi, delta);
 
-        // printf("PI = %.30g computed in %.4g seconds\n", pi, delta);
-
-    // }
+    }
     
 
 }
